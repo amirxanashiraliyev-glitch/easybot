97c191881e84e1b507bcf181da39f5d4aa0fe749
amirxanashiraliyev-glitch <amirxanashiraliyev@gmail.com>
2026-01-05 21:34:01 +0500

    Refactor bot to use environment variable for TOKEN
    Updated bot to retrieve TOKEN from environment variables and improved greeting message.

--- bot.py
+++ bot.py
@@ -1,15 +1,24 @@
+import os
 import telebot
 
-TOKEN = "8361921715:AAElpzMWptf-htcIswfWIdMs5c1JeOtvRWw"
+# TOKEN Render'dan olinadi (Environment Variable)
+TOKEN = os.environ.get("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Salom! 👋\nMenga nima yozsang, shuni qaytaraman.")
+    bot.send_message(
+        message.chat.id,
+        "Salom 👋\n"
+        "Bu mening birinchi Telegram botim.\n\n"
+        "Menga nima yozsang, shuni qaytaraman 😊"
+    )
 
-@bot.message_handler(func=lambda m: True)
+@bot.message_handler(func=lambda message: True)
 def echo(message):
     bot.send_message(message.chat.id, message.text)
 
+# Botni doimiy ishlatish
 bot.infinity_polling()
+
